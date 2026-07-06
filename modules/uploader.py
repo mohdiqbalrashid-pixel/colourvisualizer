b58eb36545e5ec7208684abdafceed14fd4c9169
mohdiqbalrashid-pixel <mohd.iqbal.rashid@gmail.com>
2026-07-06 15:33:42 +0400

    Update uploader.py

--- modules/uploader.py
+++ modules/uploader.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+from PIL import Image
+
+
+def upload_image():
+
+    uploaded = st.file_uploader(
+        "Upload customer image",
+        type=["jpg", "jpeg", "png"]
+    )
+
+    if uploaded:
+
+        image = Image.open(uploaded).convert("RGB")
+
+        st.session_state.uploaded_image = image
