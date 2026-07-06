e2684b53c7c95c5dbbfce0a5f23853b847a4ecdf
mohdiqbalrashid-pixel <mohd.iqbal.rashid@gmail.com>
2026-07-06 15:33:06 +0400

    Update session.py

--- modules/session.py
+++ modules/session.py
@@ -1,1 +1,23 @@
+import streamlit as st
 
+
+DEFAULT_STATE = {
+
+    "uploaded_image": None,
+
+    "selected_colour": None,
+
+    "selected_surface": None,
+
+    "detected_surfaces": None
+
+}
+
+
+def initialise_session():
+
+    for key, value in DEFAULT_STATE.items():
+
+        if key not in st.session_state:
+
+            st.session_state[key] = value
