e44707f0826089c3a85d106b5d03e197afc0db48
mohdiqbalrashid-pixel <mohd.iqbal.rashid@gmail.com>
2026-07-06 15:44:31 +0400

    Update segmentation.py

--- modules/segmentation.py
+++ modules/segmentation.py
@@ -4,36 +4,26 @@ import cv2
 
 def create_wall_mask(image_np, seed_point):
 
-    """
-    Expands a single click into a wall region using flood fill.
-    """
-
     h, w = image_np.shape[:2]
 
     mask = np.zeros((h + 2, w + 2), np.uint8)
 
-    floodfilled = image_np.copy()
+    bgr = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
 
     x, y = seed_point
-
-    # OpenCV floodFill requires BGR
-    bgr = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
 
     cv2.floodFill(
         bgr,
         mask,
         (x, y),
-        (255, 0, 0),
-        loDiff=(20, 20, 20),
-        upDiff=(20, 20, 20),
+        (255, 255, 255),
+        loDiff=(25, 25, 25),
+        upDiff=(25, 25, 25),
         flags=4
     )
 
-    result = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+    final_mask = mask[1:-1, 1:-1] * 255
 
-    # Extract filled region as mask
-    final_mask = mask[1:-1, 1:-1]
+    final_mask = final_mask.astype(np.uint8)
 
-    final_mask = (final_mask * 255).astype(np.uint8)
-
-    return final_mask, result
+    return final_mask
