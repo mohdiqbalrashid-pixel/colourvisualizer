0d50392bea99a62eb4128064fb14c90f17a0f0b2
mohdiqbalrashid-pixel <mohd.iqbal.rashid@gmail.com>
2026-07-06 15:33:57 +0400

    Update preview.py

--- modules/preview.py
+++ modules/preview.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+
+def build_preview():
+
+    st.subheader("Preview")
+
+    if st.session_state.uploaded_image is None:
+
+        st.info("Upload an image to begin.")
+
+        return
+
+    st.image(
+        st.session_state.uploaded_image,
+        use_container_width=True
+    )
